f66280eacd7c6ab8d08efb8c45613482114e405c
Dmitrij Motevich <motevich.dmitrij@gmail.com>
2022-05-04 11:02:31 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app">
+    <post-form 
+    @create="createPost" 
+    />
+    <post-list 
+    :posts="posts" 
+    />
+  </div>
+</template>
+// шаблоен и структура компанетна
+// интерполяция - обращение к шаблону
+// v-  слушатель событий отрибутов(дерективы)
+// v-for для работы с массивом в дерективах
+// v-bind позволяет связывать данные с конкретным компонентом
+   
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostList,
+    PostForm,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JavaScript", body: "Описание поста" },
+        { id: 2, title: "JavaScript 2", body: "Описание поста 2" },
+        { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
+        { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
+  },
+};
+</script>
+// создаем поля и модели, необходимые для описания функции
+
+   
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-block;
+}
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-model="post.body"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+// $emit сгенироровать событие под которое может подписаться родительский комитет.
+
+<style scoped>
+form {
+  display: flax;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid tomato;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 25px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 3px solid teal;
+}
+</style>
